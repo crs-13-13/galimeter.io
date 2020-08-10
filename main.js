576d782637bbaa9d1b8a578ca516c75c2a2c3c20
crs-13 <69458794+crs-13@users.noreply.github.com>
2020-08-10 18:33:52 +0530

    Update main.js
    Edited 
               =Added few new galiya

--- main.js
+++ main.js
@@ -14,14 +14,25 @@ for (item of buttons) {
     else if (buttonText == 'check') {
         switch (screenValue) {
           case 'bal':
-            screen.value = "10%";
+            screen.value = "1%";
             break;
           case 'magi':
-            screen.value = "80%";
+            screen.value = "25%";
             break;
           case 'sala':
-            screen.value = '20%';
+            screen.value = "1%";
             break;
+          case 'magir bachchha':
+            screen.value = "99%";
+            break;
+          case 'bokachoda':
+            screen.value = "5%";
+          case 'balphata':
+            screen.value = "20%";
+          case 'haramjada':
+            screen.value = "5%";
+          case 'harami':
+            screen.valur = "3%";
           default:
             screen.value = `choose the correct one!`;
             break;
